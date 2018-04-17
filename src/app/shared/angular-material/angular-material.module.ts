69cba26e6416531acf10312127e6a707901505ac
Luis Hernando Orjuela García <lorjuela@pragmaticasoftware.local>
2018-04-17 15:14:53 -0500

    Se reemplaza por un stepper la enumeración de la introducción

--- src/app/shared/angular-material/angular-material.module.ts
+++ src/app/shared/angular-material/angular-material.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule, MatMenuModule, MatToolbarModule, MatListModule } from '@angular/material';
+import { MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule, MatMenuModule, MatToolbarModule, MatListModule, MatStepperModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
@@ -7,12 +7,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     MatFormFieldModule, MatInputModule, BrowserAnimationsModule,
     MatIconModule, MatButtonModule, MatMenuModule, MatToolbarModule,
-    MatListModule
+    MatListModule, MatStepperModule
   ],
   exports: [
     MatFormFieldModule, MatInputModule, BrowserAnimationsModule,
     MatIconModule, MatButtonModule, MatMenuModule, MatToolbarModule,
-    MatListModule
+    MatListModule, MatStepperModule
   ],
   declarations: []
 })
